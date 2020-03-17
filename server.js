339de964775131a16c47a90b0314efe27c4edebb
Carlos Diaz-Padron <carlosdiazpadron@gmail.com>
2020-03-17 01:25:37 -0400

    add participants list

--- server.js
+++ server.js
@@ -3,14 +3,18 @@ const app = express();
 const path = require('path');
 const AccessToken = require('twilio').jwt.AccessToken;
 const VideoGrant = AccessToken.VideoGrant;
+const twilio = require('twilio');
 require('dotenv').config();
 
 const MAX_ALLOWED_SESSION_DURATION = 14400;
 const twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
+const twilioAuthToken = process.env.TWILIO_AUTH_TOKEN;
 const twilioApiKeySID = process.env.TWILIO_API_KEY_SID;
 const twilioApiKeySecret = process.env.TWILIO_API_KEY_SECRET;
+const client = new twilio(twilioAccountSid, twilioAuthToken);
 
 app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.json());
 
 app.get('/token', (req, res) => {
   const { identity, roomName } = req.query;
@@ -22,6 +26,12 @@ app.get('/token', (req, res) => {
   token.addGrant(videoGrant);
   res.send(token.toJwt());
   console.log(`issued token for ${identity} in room ${roomName}`);
+});
+
+app.get('/room/:room/participants', (req, res) => {
+  client.video.v1.rooms.get(req.params.room).participants.list().then(participants => {
+    res.json(participants);
+  });
 });
 
 app.get('*', (_, res) => res.sendFile(path.join(__dirname, 'build/index.html')));
--- src/components/RoomList/RoomList.tsx
+++ src/components/RoomList/RoomList.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { useAppState } from '../../state';
 import useRoomState from '../../hooks/useRoomState/useRoomState';
 import useVideoContext from '../../hooks/useVideoContext/useVideoContext';
+
+const rooms = [
+    {id: 'living', name: 'Living Room'},
+    {id: 'jackbox', name: 'Jackbox'},
+    {id: 'karaoke', name: 'Karaoke Room'},
+    {id: 'dance', name: 'Dance Floor'}
+];
 
 export default function RoomList() {
     const { user, getToken } = useAppState();
     const { isConnecting, connect, room } = useVideoContext();
     const roomState = useRoomState();
+    const [participants, setParticipants] = useState(new Map<string, any>());
 
     const switchRoom = (name: string) => {
         return (e: any) => {
@@ -17,12 +25,38 @@ export default function RoomList() {
         };
     };
 
+    // todo(carlos): we don't actually want to update DOM every response, unless there is a change
+    const getParticipants = (room: string) => {
+        fetch(`/room/${room}/participants`).then(res =>{
+            res.json().then(p => {
+                participants.set(room, p.map((p: any) => p.identity));
+                setParticipants(new Map<string, any>(participants));
+            });
+       });
+    };
+
+    useEffect(() => {
+        rooms.forEach(room => {
+            setInterval(() => getParticipants(room.id), 1000 + (Math.random()*500));
+        });
+    }, []);
+
     return (
         <ul>
-            <li><a onClick={switchRoom('living')} href=''>Living Room</a></li>
-            <li><a onClick={switchRoom('jackbox')} href=''>Jackbox Room</a></li>
-            <li><a onClick={switchRoom('karaoke')} href=''>Karaoke Room</a></li>
-            <li><a onClick={switchRoom('dance')} href=''>Dance Floor</a></li>
+            {
+                rooms.map(room => {
+                    const users = participants.get(room.id)?.map((p: string) => (
+                        <li key={p}>{p}</li>
+                    ));
+
+                    return (
+                        <li key={room.id}>
+                            <a onClick={switchRoom(room.id)} href=''>{room.name}</a>
+                            {users}
+                        </li>
+                    );
+                })
+            }
         </ul>
     );
 };
