ee9a025d974f2b45664527e3f418b4b637ecc07e
Gail Wilson <gailwilson@Gails-MacBook-Pro.local>
2020-03-20 00:03:51 -0400

    Create useCurrentRoom hook

---
+++ src/hooks/useCurrentRoom/useCurrentRoom.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+import useMapItems from '../useSync/useMapItems';
+import { useAppState } from '../../state';
+
+// Returns the object of the party room the current user is in
+export default function useCurrentRoom() {
+  const users = useMapItems('users');
+  const rooms = useMapItems('rooms');
+  const { user } = useAppState();
+  const [currentRoom, setCurrentRoom] = useState(null);
+
+  useEffect(() => {
+    if (user && users && rooms) {
+      const userRoomId = users[user.uid]?.room;
+      const newCurrentRoom = rooms[userRoomId];
+      setCurrentRoom(newCurrentRoom);
+    }
+  }, [rooms, user, users]);
+
+  return currentRoom;
+}
