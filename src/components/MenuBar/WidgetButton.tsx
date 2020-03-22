33273455c1dbf8ff7c74d25e94f8200b7a8aa640
Carlos Diaz-Padron <carlosdiazpadron@gmail.com>
2020-03-21 21:08:43 -0400

    add widget selector modal

--- src/components/MenuBar/WidgetButton.tsx
+++ src/components/MenuBar/WidgetButton.tsx
@@ -1,21 +1,82 @@
-import React, { useCallback } from 'react';
-import { IconButton } from '@material-ui/core';
+import React, { useCallback, useState } from 'react';
+import { IconButton, Modal, styled } from '@material-ui/core';
 import { Casino, CasinoOutlined } from '@material-ui/icons';
 import useApi from '../../hooks/useApi/useApi';
 import useCurrentRoom from '../../hooks/useCurrentRoom/useCurrentRoom';
+import registry, { WidgetRegistration } from '../../widgetRegistry';
+
+const SelectorContainer = styled('div')(({ theme }) => ({
+  position: 'fixed',
+  top: '50%',
+  left: '50%',
+  backgroundColor: theme.alternateBackgroundColor,
+  width: '400px',
+  height: '500px',
+  marginTop: '-250px',
+  marginLeft: '-200px',
+  display: 'flex',
+  flexDirection: 'column',
+  padding: '25px',
+  outline: 'none',
+}));
+
+const Title = styled('p')({
+  fontSize: '24px',
+  fontWeight: 'bold',
+  marginTop: 0,
+  marginBottom: '16px',
+});
+
+const WidgetInfoContainer = styled('div')(({ theme }) => ({
+  cursor: 'pointer',
+  backgroundColor: theme.palette.background.paper,
+  padding: '16px',
+}));
+
+const WidgetInfoTitle = styled('p')(({ theme }) => ({
+  fontSize: '18px',
+  fontWeight: 600,
+}));
+
+const WidgetInfoDescription = styled('p')(({ theme }) => ({}));
+
+interface WidgetInfoProps {
+  id: string;
+  widget: WidgetRegistration;
+  onClick: (id: string) => void;
+}
+
+const WidgetInfo = (props: WidgetInfoProps) => {
+  const onClick = useCallback(() => {
+    props.onClick(props.id);
+  }, [props]);
+
+  return (
+    <WidgetInfoContainer onClick={onClick}>
+      <WidgetInfoTitle>{props.widget.name}</WidgetInfoTitle>
+      <WidgetInfoDescription>{props.widget.description}</WidgetInfoDescription>
+    </WidgetInfoContainer>
+  );
+};
 
 export default function WidgetButton() {
   const { callApi } = useApi();
   const room = useCurrentRoom();
+  const [open, setOpen] = useState(false);
 
-  const select = useCallback(() => {
-    if (room && !room.widgetId) {
-      callApi('create_widget_state', {
-        roomId: room.id,
-        widgetId: 'test',
-      }).catch(e => console.error(e));
-    }
-  }, [callApi, room]);
+  const select = useCallback(
+    (widgetId: string) => {
+      if (room && !room.widgetId) {
+        callApi('create_widget_state', {
+          roomId: room.id,
+          widgetId,
+        }).catch(e => console.error(e));
+      }
+
+      setOpen(false);
+    },
+    [callApi, room]
+  );
 
   const remove = useCallback(() => {
     if (room) {
@@ -25,6 +86,16 @@ export default function WidgetButton() {
     }
   }, [callApi, room]);
 
+  const openSelector = useCallback(() => {
+    setOpen(true);
+  }, [setOpen]);
+
+  var widgets = [];
+
+  for (const id in registry) {
+    widgets.push(<WidgetInfo id={id} widget={registry[id]} onClick={select} />);
+  }
+
   return (
     <div>
       {room?.widgetId ? (
@@ -32,10 +103,16 @@ export default function WidgetButton() {
           <CasinoOutlined />
         </IconButton>
       ) : (
-        <IconButton onClick={select}>
+        <IconButton onClick={openSelector}>
           <Casino />
         </IconButton>
       )}
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <SelectorContainer>
+          <Title>Select a game to play!</Title>
+          {widgets}
+        </SelectorContainer>
+      </Modal>
     </div>
   );
 }
--- src/widgetRegistry.ts
+++ src/widgetRegistry.ts
@@ -2,17 +2,19 @@ import { FunctionComponent } from 'react';
 import TestWidget from './components/Widgets/TestWidget';
 
 export interface WidgetRegistration {
-    name: string;
-    component: FunctionComponent;
+  name: string;
+  component: FunctionComponent;
+  description: string;
 }
 
 export interface WidgetRegistry {
-    [key: string]: WidgetRegistration;
+  [key: string]: WidgetRegistration;
 }
 
 export default {
-    test: {
-        name: 'Test Widget',
-        component: TestWidget,
-    },
+  test: {
+    name: 'Test Widget',
+    component: TestWidget,
+    description: 'A test widget, temporary',
+  },
 } as WidgetRegistry;
