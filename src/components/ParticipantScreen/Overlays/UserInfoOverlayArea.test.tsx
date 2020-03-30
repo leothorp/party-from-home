dd253f4fc31158cb17a70dda546b4500ec895b5a
Gail Wilson <gailwilson@Gails-MacBook-Pro.local>
2020-03-29 20:01:42 -0400

    fix a test

--- src/components/ParticipantScreen/Overlays/UserInfoOverlayArea.test.tsx
+++ src/components/ParticipantScreen/Overlays/UserInfoOverlayArea.test.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import UserInfoOverlayArea from './UserInfoOverlayArea';
 import UserInfoOverlay from './UserInfoOverlays/UserInfoOverlay';
+import { USER_INFO_OVERLAY_TYPES } from '../../../constants/registryContants';
+import userInfoOverlayRegistry from '../../../registries/userInfoOverlayRegistry';
 
 describe('the ParticipantInfo component', () => {
   const mockIdentity = { identity: 'mockIdentity' };
   const defaultProps = {
     participant: mockIdentity as any,
+    overlays: [
+      userInfoOverlayRegistry[USER_INFO_OVERLAY_TYPES.USER_AND_CONNECTION_INFO],
+      userInfoOverlayRegistry[USER_INFO_OVERLAY_TYPES.NETWORK_QUALITY_LEVEL],
+      userInfoOverlayRegistry[USER_INFO_OVERLAY_TYPES.VIDEO_AND_AUDIO_INFO],
+      userInfoOverlayRegistry[USER_INFO_OVERLAY_TYPES.PIN_ICON],
+    ],
   };
   const getWrapper = (overrides?: object) => shallow(<UserInfoOverlayArea {...defaultProps} {...overrides} />);
 
