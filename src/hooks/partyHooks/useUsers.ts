2b166e20e02c3d2a0db788d4d85350fb358936a3
Carlos Diaz-Padron <carlosdiazpadron@gmail.com>
2020-04-01 18:18:46 -0400

    add user subscriptions

--- src/hooks/partyHooks/useUsers.ts
+++ src/hooks/partyHooks/useUsers.ts
@@ -1,18 +1,71 @@
 import gql from 'graphql-tag';
 import { useQuery } from '@apollo/react-hooks';
+import useMountEffect from '../useMountEffect/useMountEffect';
 
 const QUERY = gql`
-    {
-        users {
-            identity
-            displayName
-            photoURL
-        }
+  {
+    users {
+      identity
+      displayName
+      photoURL
     }
+  }
+`;
+
+const NEW_USER = gql`
+  subscription onNewUser {
+    newUser {
+      user {
+        identity
+        displayName
+        photoURL
+      }
+    }
+  }
+`;
+
+const DELETED_USER = gql`
+  subscription onDeletedUser {
+    deletedUser {
+      identity
+    }
+  }
 `;
 
 export default function useUsers() {
-    const { data } = useQuery(QUERY);
+  const { data, subscribeToMore } = useQuery(QUERY);
 
-    return { users: (data?.users || []) as any[] };
+  useMountEffect(() => {
+    subscribeToMore({
+      document: NEW_USER,
+      updateQuery: (prev, { subscriptionData }) => {
+        if (!subscriptionData || !prev) return prev;
+
+        const newUser = subscriptionData.data.newUser.user;
+
+        if (prev.users.find((u: any) => u.identity === newUser.identity)) return prev;
+
+        return {
+          ...prev,
+          users: [...prev.users, newUser],
+        };
+      },
+    });
+
+    subscribeToMore({
+      document: DELETED_USER,
+      updateQuery: (prev, { subscriptionData }) => {
+        if (!subscriptionData || !prev) return prev;
+
+        const userId = subscriptionData.data.deletedUser.identity;
+
+        return {
+          ...prev,
+          users: prev.users.filter((u: any) => u.identity === userId),
+        };
+      },
+    });
+  });
+
+  return { users: (data?.users || []) as any[] };
 }
--- src/server/resolvers/users.ts
+++ src/server/resolvers/users.ts
@@ -1,7 +1,34 @@
 import { PartyUser } from '../db';
-import { Resolver, Query, Mutation, Authorized, Arg, Ctx } from 'type-graphql';
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Subscription,
+  Authorized,
+  Arg,
+  Ctx,
+  Root,
+  PubSub,
+  PubSubEngine,
+  ObjectType,
+  Field,
+} from 'type-graphql';
 import { RequestContext } from '../context';
 import { ENV } from '../config';
+
+@ObjectType()
+class UserNotification {
+  @Field()
+  identity!: string;
+  @Field()
+  user!: PartyUser;
+}
+
+@ObjectType()
+class UserDeleteNotification {
+  @Field()
+  identity!: string;
+}
 
 @Resolver(PartyUser)
 export default class PartyUserResolver {
@@ -11,23 +38,43 @@ export default class PartyUserResolver {
   }
 
   @Query(_returns => PartyUser)
-  async user(@Arg("identity") identity: string, @Ctx() { db }: RequestContext) {
+  async user(@Arg('identity') identity: string, @Ctx() { db }: RequestContext) {
     return db.getUser(identity);
   }
 
   @Mutation(_returns => PartyUser)
   async register(
     @Ctx() { db }: RequestContext,
-    @Arg("identity") identity: string,
-    @Arg("displayName") displayName: string,
-    @Arg("photoURL", { nullable: true }) photoURL?: string,
+    @PubSub() pubsub: PubSubEngine,
+    @Arg('identity') identity: string,
+    @Arg('displayName') displayName: string,
+    @Arg('photoURL', { nullable: true }) photoURL?: string
   ): Promise<PartyUser> {
     const token = ENV === 'production' ? undefined : 'h9d7d9g';
-    return db.addUser({
+    const user = await db.addUser({
       identity,
       displayName,
       photoURL,
       token,
     });
+
+    await pubsub.publish('CREATE_USER', { identity: user.identity, user });
+
+    return user;
+  }
+
+  @Subscription({ topics: 'CREATE_USER' })
+  newUser(@Root() payload: UserNotification): UserNotification {
+    return payload;
+  }
+
+  @Subscription({ topics: 'UPDATE_USER' })
+  updatedUser(@Root() payload: UserNotification): UserNotification {
+    return payload;
+  }
+
+  @Subscription({ topics: 'DELETED_USER' })
+  deletedUser(@Root() payload: UserDeleteNotification): UserDeleteNotification {
+    return payload;
   }
 }
